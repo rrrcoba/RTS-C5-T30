3f42ee8ca38ed5e287aba38637be9dd944352e16
rrrcoba <roger-decroly@hotmail.com>
2022-08-30 14:01:31 +0200

    Exercise 8 finished

---
+++ 8/js/code.js
@@ -0,0 +1,7 @@
+let n = prompt("Introduce un número que sepas saber si es par o impar: ")
+
+if (n % 2 == 0) {
+    alert("El número introducido es par.");
+} else {
+    alert("El número introducido es impar.");
+}
