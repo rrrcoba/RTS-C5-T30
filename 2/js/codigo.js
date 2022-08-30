e124bef4082b2afb56c39fce473d57a35c6eb4c1
rrrcoba <roger-decroly@hotmail.com>
2022-08-30 11:40:07 +0200

    Exercise 2 finished

---
+++ 2/js/codigo.js
@@ -0,0 +1,8 @@
+// Muestra el siguiente mensaje por consolta
+console.log("Hola Mundo!");
+
+// La función crea un mensaje que luego muestra en una alerta
+function myAlert() {
+    let mensaje = "Hola Mundo!\nQué fácil es incluir 'comillas simples'\ny \"comillas dobles\"";
+    alert(mensaje);
+}
