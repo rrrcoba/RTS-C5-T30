db7d34b870511bd7ca1aea67d58167eacf6fd78f
rrrcoba <roger-decroly@hotmail.com>
2022-08-30 12:22:52 +0200

    Exercise 3 finished

---
+++ 3/js/code.js
@@ -0,0 +1,5 @@
+let months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+for (let i = 0; i < months.length; i++) {
+    console.log(months[i]);
+}
