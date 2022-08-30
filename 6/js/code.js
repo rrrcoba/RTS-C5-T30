f4cb2b1eb7b6bd71601a04fc7597d526077a4a12
rrrcoba <roger-decroly@hotmail.com>
2022-08-30 13:19:07 +0200

    Exercise 6 finished

---
+++ 6/js/code.js
@@ -0,0 +1,18 @@
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+
+
+let n = prompt("Introduce tu número de DNI sin letra: ");
+let len = n.length;
+if (len != 8 || n < 0 || n > 99999999) {
+    alert("El dni introducido no es válido.");
+}
+
+let letter = prompt("Introduce la letra de tu DNI en mayúsculas: ");
+letter.toUpperCase();
+let rest = n%23;
+let calculatedLetter = letras[rest];
+if (calculatedLetter != letter) {
+    alert("La letra que has introducido no es correcta.");
+} else {
+    alert("DNI Validado correctamente");
+}
